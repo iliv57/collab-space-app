519df9bd9a3c1c6808b0c6d49d2243526b810c4a
Marius Friess <=>
2023-06-26 14:09:12 +0200

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,9 +3,15 @@
 cp ./backend/.env.docker.example ./backend/.env
 cp ./frontend/.env.example ./frontend/.env
 
+# Erstelle eine neue Zeile in .env
+echo >> ./frontend/.env
+
 # Ueberpruefe, ob das Betriebssystem Windows ist
-if [ "$(uname -s)" = "MINGW"* ] || [ "$(uname -s)" = "CYGWIN"* ] || [ "$(uname -s)" = "MSYS"* ]; then
-    echo '\nCHOKIDAR_USEPOLLING=true' >> ./frontend/.env
+# Falls ja, dann setze CHOKIDAR_USEPOLLING=true
+# Falls nein, dann setze CHOKIDAR_USEPOLLING=false
+# Setze CHOKIDAR_USEPOLLING in .env in eine neue Zeile
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  echo "CHOKIDAR_USEPOLLING=true" >> ./frontend/.env
 else
-    echo '\nCHOKIDAR_USEPOLLING=false' >> ./frontend/.env
+  echo "CHOKIDAR_USEPOLLING=false" >> ./frontend/.env
 fi

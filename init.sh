b385be3952049a87041fa43ff6e19cf028a57ed8
Marius Friess <34072851+mariusfriess@users.noreply.github.com>
2023-06-29 14:35:51 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,16 +2,3 @@
 
 cp ./backend/.env.docker.example ./backend/.env
 cp ./frontend/.env.example ./frontend/.env
-
-# Erstelle eine neue Zeile in .env
-echo >> ./frontend/.env
-
-# Ueberpruefe, ob das Betriebssystem Windows ist
-# Falls ja, dann setze CHOKIDAR_USEPOLLING=true
-# Falls nein, dann setze CHOKIDAR_USEPOLLING=false
-# Setze CHOKIDAR_USEPOLLING in .env in eine neue Zeile
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-  echo "CHOKIDAR_USEPOLLING=true" >> ./frontend/.env
-else
-  echo "CHOKIDAR_USEPOLLING=false" >> ./frontend/.env
-fi
